784be232562be96343d3d307f6b7c5dbbcddd4b2
Andrei <skibin-03@mail.ru>
2023-07-30 19:34:41 +0500

    Separated host and port for db urls, updated requirements.txt

--- db.py
+++ db.py
@@ -21,12 +21,14 @@ class Database:
             db_user = os.getenv("db_user_test")
             db_password = os.getenv("db_password_test")
             db_host = os.getenv("db_host_test")
+            db_port = os.getenv("db_port_test")
             db_name = os.getenv("db_name_test")
         else:
             db_user = os.getenv("db_user")
             db_password = os.getenv("db_password")
             db_host = os.getenv("db_host")
+            db_port = os.getenv("db_port")
             db_name = os.getenv("db_name")
 
-        self.engine = create_async_engine(f"postgresql+asyncpg://{db_user}:{db_password}@{db_host}/{db_name}")
+        self.engine = create_async_engine(f"postgresql+asyncpg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
         self.session_maker = async_sessionmaker(self.engine, class_=AsyncSession)
